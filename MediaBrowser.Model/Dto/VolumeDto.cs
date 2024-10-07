4351127dca03fa2c3a979fae6a94300d43706bae
Rasmus Bengtsson <rasmus@beryllium-tech.eu>
2024-10-07 13:02:21 +0200

    feat: created a storage volume DTO

---
+++ MediaBrowser.Model/Dto/VolumeDto.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+
+namespace MediaBrowser.Model.Dto;
+
+/// <summary>
+/// A data transfer object to represent a storage volume.
+/// </summary>
+public class VolumeDto
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="VolumeDto"/> class.
+    /// </summary>
+    /// <param name="volumeIdentifier">The identifier of the volume.</param>
+    public VolumeDto(string volumeIdentifier)
+    {
+        VolumeIdentifier = volumeIdentifier;
+        LibraryIdentifiers = new List<string>();
+    }
+
+    /// <summary>
+    /// Gets or sets the identifier of storage volume, on linux most often /dev/sdx.
+    /// </summary>
+    public string VolumeIdentifier { get; set; }
+
+    /// <summary>
+    /// Gets or sets the space remaining on the specific volume.
+    /// </summary>
+    public int SpaceRemaining { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of LibraryIdentifiers stored on the volume.
+    /// </summary>
+    public IEnumerable<string> LibraryIdentifiers { get; set; }
+}
